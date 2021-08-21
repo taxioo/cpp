a7bd4375f0f677f6f3caaa2459da5128a05c9b91
taxioo <taxioo@naver.com>
2021-08-22 00:18:00 +0900

    ex01 추가

--- chapter01/ex01.cpp
+++ chapter01/ex01.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
+
 int main(int argc, char const *argv[])
-{
- cout << "Hello World" << endl;
- // std::cout << "Hello World" << std::endl;
- return 0;
-}
+    {
+    cout << "Hello World" << endl;
+    // std::cout << "Hello World" << std::endl;
+    return 0; // 파이썬: sys.exit(0)
+    }
